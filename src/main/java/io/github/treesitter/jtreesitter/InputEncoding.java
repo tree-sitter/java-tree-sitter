5c467db15348f73dc5fd6dfe1ad5aa580fcbe456
Samuel Rother <35152484+Sigma42@users.noreply.github.com>
2024-10-01 09:48:28 +0000

    Fix UTF16 byte order + extend utf8 & utf16 test

--- src/main/java/io/github/treesitter/jtreesitter/InputEncoding.java
+++ src/main/java/io/github/treesitter/jtreesitter/InputEncoding.java
@@ -1,5 +1,6 @@
 package io.github.treesitter.jtreesitter;
 
+import java.nio.ByteOrder;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import org.jspecify.annotations.NonNull;
@@ -9,7 +10,7 @@ public enum InputEncoding {
     /** UTF-8 encoding. */
     UTF_8(StandardCharsets.UTF_8),
     /** UTF-16 encoding. */
-    UTF_16(StandardCharsets.UTF_16BE);
+    UTF_16(ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN ? StandardCharsets.UTF_16BE : StandardCharsets.UTF_16LE);
 
     private final @NonNull Charset charset;
 
@@ -24,12 +25,14 @@ public enum InputEncoding {
     /**
      * Convert a standard {@linkplain Charset} to an {@linkplain InputEncoding}.
      *
-     * @param charset one of {@link StandardCharsets#UTF_8} or {@link StandardCharsets#UTF_16BE}
+     * @param charset one of {@link StandardCharsets#UTF_8} or {@link StandardCharsets#UTF_16} ({@link StandardCharsets#UTF_16LE UTF_16LE} and {@link StandardCharsets#UTF_16BE UTF_16BE} will work too, but native byte order will be used)
      * @throws IllegalArgumentException If the character set is invalid.
      */
     static @NonNull InputEncoding valueOf(@NonNull Charset charset) throws IllegalArgumentException {
         if (charset.equals(StandardCharsets.UTF_8)) return InputEncoding.UTF_8;
-        if (charset.equals(StandardCharsets.UTF_16BE)) return InputEncoding.UTF_16;
+        if (charset.equals(StandardCharsets.UTF_16BE)
+                || charset.equals(StandardCharsets.UTF_16LE)
+                || charset.equals(StandardCharsets.UTF_16)) return InputEncoding.UTF_16;
         throw new IllegalArgumentException("Invalid character set: %s".formatted(charset));
     }
 }
