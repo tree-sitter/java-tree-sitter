6ae5953fffb50995cde6fd547c0511b95b581187
odenix <self@odenix.org>
2024-12-05 20:28:24 -0800

    feat: support custom SymbolLookup for the library
    Enable clients to customize the SymbolLookup used for the tree-sitter native library.
    For example, clients can use this capability to embed the tree-sitter native library
    in their JAR, which is a common way to solve the native library distribution problem.

---
+++ src/main/java/io/github/treesitter/jtreesitter/internal/ChainedLibraryLookup.java
@@ -0,0 +1,35 @@
+package io.github.treesitter.jtreesitter.internal;
+
+import io.github.treesitter.jtreesitter.NativeLibraryLookup;
+import java.lang.foreign.Arena;
+import java.lang.foreign.Linker;
+import java.lang.foreign.SymbolLookup;
+import java.util.Optional;
+import java.util.ServiceLoader;
+
+@SuppressWarnings("unused")
+final class ChainedLibraryLookup implements NativeLibraryLookup {
+    private ChainedLibraryLookup() {}
+
+    static ChainedLibraryLookup INSTANCE = new ChainedLibraryLookup();
+
+    @Override
+    public SymbolLookup get(Arena arena) {
+        var serviceLoader = ServiceLoader.load(NativeLibraryLookup.class);
+        // NOTE: can't use _ because of palantir/palantir-java-format#934
+        SymbolLookup lookup = (name) -> Optional.empty();
+        for (var libraryLookup : serviceLoader) {
+            lookup = lookup.or(libraryLookup.get(arena));
+        }
+        return lookup.or(findLibrary(arena)).or(Linker.nativeLinker().defaultLookup());
+    }
+
+    private static SymbolLookup findLibrary(Arena arena) {
+        try {
+            var library = System.mapLibraryName("tree-sitter");
+            return SymbolLookup.libraryLookup(library, arena);
+        } catch (IllegalArgumentException e) {
+            return SymbolLookup.loaderLookup();
+        }
+    }
+}
