deb8abd690848de097f3d573491e967d53af03a8
ObserverOfTime <chronobserver@disroot.org>
2025-08-14 11:20:42 +0300

    feat!: don't extend BiConsumer/BiFunction

--- src/main/java/io/github/treesitter/jtreesitter/ParseCallback.java
+++ src/main/java/io/github/treesitter/jtreesitter/ParseCallback.java
@@ -1,20 +1,32 @@
 package io.github.treesitter.jtreesitter;
 
-import java.util.function.BiFunction;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
 /** A function that retrieves a chunk of text at a given byte offset and point. */
 @FunctionalInterface
-public interface ParseCallback extends BiFunction<Integer, Point, String> {
+public interface ParseCallback {
     /**
-     * {@inheritDoc}
+     * Applies this function to the given arguments.
      *
      * @param offset the current byte offset
      * @param point the current point
      * @return A chunk of text or {@code null} to indicate the end of the document.
      */
-    @Override
     @Nullable
-    String apply(@Unsigned Integer offset, @NonNull Point point);
+    String read(@Unsigned int offset, @NonNull Point point);
+
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param offset the current byte offset
+     * @param point the current point
+     * @return A chunk of text or {@code null} to indicate the end of the document.
+     * @deprecated Use {@link #read(int, Point)} instead
+     */
+    @Nullable
+    @Deprecated(since = "0.26.0", forRemoval = true)
+    default String apply(@Unsigned Integer offset, @NonNull Point point) {
+        return read(offset, point);
+    }
 }
